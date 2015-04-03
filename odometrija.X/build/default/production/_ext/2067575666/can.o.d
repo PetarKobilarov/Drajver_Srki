4e16c1ef677900f76e912f356a00e616d448054a
Petar Kobilarov <pkobilarov96@gmail.com>
2015-04-03 18:37:41 +0200

    Neki kod za drajver sto mi je Srki poslao,nije imao za VHN ali je sava nesto budzio na njemu.

---
+++ odometrija.X/build/default/production/_ext/2067575666/can.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/2067575666/can.o:  \
+ ../../../../Robotika\ 2014\ i\ ranije/KodoviZaVelikog2014/veliki_driver/driver\ 1.0/odometrija2012/can.c  \
+ ../../../../Robotika\ 2014\ i\ ranije/KodoviZaVelikog2014/veliki_driver/driver\ 1.0/odometrija2012/can.h 
